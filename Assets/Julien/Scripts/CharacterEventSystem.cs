54c0a48c208f9bb9e9f96609b2c7d6585c73549b
Julien <julien.delaunay034@gmail.com>
2019-10-27 02:17:17 +0200

    Scene animation ready

---
+++ Assets/Julien/Scripts/CharacterEventSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterEventSystem : MonoBehaviour
+{
+	public Animation _anim;
+	public GameManager gameManager;
+
+
+	public void StartGame()
+	{
+		_anim.Play();
+	}
+
+	private void Ready()
+	{
+		gameManager._onBeingReady?.Invoke();
+	}
+}
---
+++ Assets/Julien/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+	public CharacterEventSystem character;
+	private bool _gameStarted;
+	public UnityEvent _onBeingReady;
+
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && !_gameStarted)
+		{
+			character.StartGame();
+		}
+	}
+}
